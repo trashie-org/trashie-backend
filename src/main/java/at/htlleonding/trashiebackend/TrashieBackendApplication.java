dfaffd51cc058e6d932d801f84d3751721b01d74
kxrim <k.yagci@students.htl-leonding.ac.at>
2026-09-22 10:00:36 +0200

    initial commit

---
+++ src/main/java/at/htlleonding/trashiebackend/TrashieBackendApplication.java
@@ -0,0 +1,13 @@
+package at.htlleonding.trashiebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrashieBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrashieBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/htlleonding/trashiebackend/TrashieBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.htlleonding.trashiebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrashieBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
